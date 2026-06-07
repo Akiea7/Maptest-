1fae4eb472494d0eb99b754b647ef47ea322b026
Akiea7 <pand183876@gmail.com>
2026-06-07 16:13:59 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// إضافة تفاعل بسيط عند الضغط على زر "وقع العقد"
+document.getElementById('explore-btn').addEventListener('click', function(e) {
+    // سنقوم بإضافة تأثيرات أنيميشن قوية هنا في الخطوات القادمة
+    console.log("تم البدء في استكشاف عالم فاوستس المظلم...");
+});
