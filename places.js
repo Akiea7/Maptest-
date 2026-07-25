fa5c574600e9bd334a683baeabdea6d4312f3a19
Akiea7 <pand183876@gmail.com>
2026-07-26 01:05:06 +0300

    Update places.js

--- places.js
+++ places.js
@@ -1,29 +1,24 @@
-        {"type":"Feature","properties":{"title":"ملعب جديدة الشط الأولمبي","icon":"pitch","priority":5},"geometry":{"type":"Point","coordinates":[44.4276334,33.6685103]}},
-        
-        {"type":"Feature","properties":{"title":"مكتب حجي محمود للعقار","icon":"commercial","priority":5},"geometry":{"type":"Point","coordinates":[44.3808122,33.6666931]}},
-        {"type":"Feature","properties":{"title":"مكتب ابو ياسر للعقار","icon":"commercial","priority":5},"geometry":{"type":"Point","coordinates":[44.3785603,33.6666402]}},
-        {"type":"Feature","properties":{"title":"مصرف الطارمية الزراعي","icon":"bank","priority":3},"geometry":{"type":"Point","coordinates":[44.3772711,33.6671201]}},
-        {"type":"Feature","properties":{"title":"محطة مياه الفرات","icon":"water","priority":5},"geometry":{"type":"Point","coordinates":[44.3793106,33.673189]}},
-        
-        {"type":"Feature","properties":{"title":"خلفة سيراميك","icon":"hardware","priority":5},"geometry":{"type":"Point","coordinates":[44.4154122,33.6853471]}},
-        {"type":"Feature","properties":{"title":"سكلة حسين النمراؤي","icon":"hardware","priority":5},"geometry":{"type":"Point","coordinates":[44.372863,33.6738513]}},
-        {"type":"Feature","properties":{"title":"مصعب حميد مولده","icon":"hardware","priority":5},"geometry":{"type":"Point","coordinates":[44.3805975,33.672222]}},
-        {"type":"Feature","properties":{"title":"ياسر العبيدي لعمل السيراميك","icon":"hardware","priority":5},"geometry":{"type":"Point","coordinates":[44.377908,33.679692]}},
-        {"type":"Feature","properties":{"title":"عمار ابو فنن المشهداني","icon":"hardware","priority":5},"geometry":{"type":"Point","coordinates":[44.3858861,33.6718207]}},
-        {"type":"Feature","properties":{"title":"انشائية فدك الزهراء","icon":"hardware","priority":5},"geometry":{"type":"Point","coordinates":[44.4107387,33.6517415]}},
-        {"type":"Feature","properties":{"title":"سكلة الوسيله للبيع الحديد","icon":"hardware","priority":5},"geometry":{"type":"Point","coordinates":[44.3933912,33.6608456]}},
-        {"type":"Feature","properties":{"title":"سكلة ابو عباس","icon":"hardware","priority":5},"geometry":{"type":"Point","coordinates":[44.3691163,33.6613616]}},
-        {"type":"Feature","properties":{"title":"سكله ابو يمان","icon":"hardware","priority":5},"geometry":{"type":"Point","coordinates":[44.3684898,33.6631848]}},
+// =========================================================
+// 📍 ملف places.js - قاعدة بيانات الأماكن (نقاط الاهتمام)
+// =========================================================
 
-        {"type":"Feature","properties":{"title":"طارمية","icon":"marker","priority":5},"geometry":{"type":"Point","coordinates":[44.3783246,33.6668412]}},
-        {"type":"Feature","properties":{"title":"ابو ياسر","icon":"marker","priority":5},"geometry":{"type":"Point","coordinates":[44.4000887,33.6356952]}},
-        {"type":"Feature","properties":{"title":"ابو نور","icon":"marker","priority":5},"geometry":{"type":"Point","coordinates":[44.4205335,33.6847788]}},
-        {"type":"Feature","properties":{"title":"مثلث الذهبي","icon":"marker","priority":5},"geometry":{"type":"Point","coordinates":[44.4246454,33.6823424]}},
-        {"type":"Feature","properties":{"title":"شارع الناحية","icon":"marker","priority":5},"geometry":{"type":"Point","coordinates":[44.3936342,33.6649699]}},
-        {"type":"Feature","properties":{"title":"الجوهرة للأحذية والحقائب","icon":"marker","priority":5},"geometry":{"type":"Point","coordinates":[44.3858717,33.6626517]}},
-        {"type":"Feature","properties":{"title":"الحجازي","icon":"marker","priority":5},"geometry":{"type":"Point","coordinates":[44.3977724,33.6646279]}},
-        {"type":"Feature","properties":{"title":"قرب جامع ابو عبيدة","icon":"marker","priority":5},"geometry":{"type":"Point","coordinates":[44.3812656,33.6703375]}},
-        {"type":"Feature","properties":{"title":"الحياليين","icon":"marker","priority":5},"geometry":{"type":"Point","coordinates":[44.3789695,33.6714024]}},
-        {"type":"Feature","properties":{"title":"احمد سمير","icon":"marker","priority":5},"geometry":{"type":"Point","coordinates":[44.3800174,33.6776095]}}
+const placesData = {
+    "type": "FeatureCollection",
+    "features": [
+        {"type":"Feature","properties":{"title":"ملعب جديدة الشط الأولمبي","icon":"pitch_11"},"geometry":{"type":"Point","coordinates":[44.4276334,33.6685103]}},
+        {"type":"Feature","properties":{"title":"مكتب حجي محمود للعقار","icon":"commercial_11"},"geometry":{"type":"Point","coordinates":[44.3808122,33.6666931]}},
+        {"type":"Feature","properties":{"title":"مكتب ابو ياسر للعقار","icon":"commercial_11"},"geometry":{"type":"Point","coordinates":[44.3785603,33.6666402]}},
+        {"type":"Feature","properties":{"title":"مصرف الطارمية الزراعي","icon":"bank_11"},"geometry":{"type":"Point","coordinates":[44.3772711,33.6671201]}},
+        {"type":"Feature","properties":{"title":"محطة مياه الفرات","icon":"water_11"},"geometry":{"type":"Point","coordinates":[44.3793106,33.673189]}},
+        {"type":"Feature","properties":{"title":"خلفة سيراميك","icon":"hardware_11"},"geometry":{"type":"Point","coordinates":[44.4154122,33.6853471]}},
+        {"type":"Feature","properties":{"title":"سكلة حسين النمراوي","icon":"hardware_11"},"geometry":{"type":"Point","coordinates":[44.372863,33.6738513]}},
+        {"type":"Feature","properties":{"title":"مصعب حميد مولده","icon":"hardware_11"},"geometry":{"type":"Point","coordinates":[44.3805975,33.672222]}},
+        {"type":"Feature","properties":{"title":"ياسر العبيدي لعمل السيراميك","icon":"hardware_11"},"geometry":{"type":"Point","coordinates":[44.377908,33.679692]}},
+        {"type":"Feature","properties":{"title":"سكلة الوسيله لبيع الحديد","icon":"hardware_11"},"geometry":{"type":"Point","coordinates":[44.3933912,33.6608456]}},
+        {"type":"Feature","properties":{"title":"سكلة ابو عباس","icon":"hardware_11"},"geometry":{"type":"Point","coordinates":[44.3691163,33.6613616]}},
+        {"type":"Feature","properties":{"title":"طارمية","icon":"marker_15"},"geometry":{"type":"Point","coordinates":[44.3783246,33.6668412]}},
+        {"type":"Feature","properties":{"title":"الجوهرة للأحذية والحقائب","icon":"shop_11"},"geometry":{"type":"Point","coordinates":[44.3858717,33.6626517]}},
+        {"type":"Feature","properties":{"title":"قرب جامع ابو عبيدة","icon":"place_of_worship_11"},"geometry":{"type":"Point","coordinates":[44.3812656,33.6703375]}},
+        {"type":"Feature","properties":{"title":"الحياليين","icon":"marker_15"},"geometry":{"type":"Point","coordinates":[44.3789695,33.6714024]}}
     ]
 };
